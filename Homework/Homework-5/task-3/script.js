9d4ef78c935abb1c7747d155e3d2696e1b05d598
Vitaly <vitaly.zutov@yandex.ru>
2024-04-06 19:09:08 +0700

    Homework-5. Objects in JavaScript

---
+++ Homework/Homework-5/task-3/script.js
@@ -0,0 +1,24 @@
+/* Задание 3
+Дан массив products, необходимо цену каждого продукта уменьшить на 15% 
+используя метод forEach. */
+
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+products.forEach(product => {
+    product.price = Math.floor(product.price * 0.85)
+});
+
+console.log(products);
