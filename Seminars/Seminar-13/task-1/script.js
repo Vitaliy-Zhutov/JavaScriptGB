39492d6d3b153c73405832cd1cb0b2ce97d97bd2
Vitaly <vitaly.zutov@yandex.ru>
2024-05-12 21:57:31 +0700

    Seminar-13. Dom tree. JavaScript

---
+++ Seminars/Seminar-13/task-1/script.js
@@ -0,0 +1,47 @@
+/* Задание 1
+Работа с BOM
+1. Определение текущего размера окна браузера:
+- Напишите функцию, которая будет выводить текущую
+ширину и высоту окна браузера при его изменении.
+2. Подтверждение закрытия страницы:
+- Создайте всплывающее окно или диалоговое окно,
+которое появляется при попытке закрыть вкладку
+браузера и спрашивает пользователя, уверен ли он в
+своем решении закрыть страницу.
+3. Управление историей переходов:
+- Используйте объект history для управления историей
+переходов на веб-странице. Создайте кнопки "Назад" и
+"Вперед" для перемещения по истории. */
+
+//////////////////////////////// 1
+
+// const updateWindowSize = () => {
+//     const widthEl = window.innerWidth;
+//     const heightEl = window.innerHeight;
+
+//     const sindowSizeElement = document.querySelectorAll('.window-size');
+    
+//     width.textContent = widthEl;
+//     height.textContent = heightEl;
+// }
+
+// window.addEventListener('load', updateWindowSize);
+// window.addEventListener('resize', updateWindowSize);
+
+////////////////////////////////// 2
+
+// window.addEventListener('beforeunload', function (e) {
+//     e.preventDefault();
+//     e.returnValue = 'Text';
+// });
+
+///////////////////////////////// 3
+
+goBack.addEventListener('load', function (e) {
+    window.history.back();
+});
+
+goForward.addEventListener('load', function (e) {
+    window.history.forward();
+});
+
